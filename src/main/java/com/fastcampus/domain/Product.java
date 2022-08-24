0efdb852f24a713d3893898aa2fd2c1235c94d3a
ddungi <imbc0224@naver.com>
2022-08-24 18:08:35 +0900

    feat : product 도메인 생성

--- src/main/java/com/fastcampus/domain/Product.java
+++ src/main/java/com/fastcampus/domain/Product.java
@@ -1,5 +1,27 @@
 package com.fastcampus.domain;
 
-public class Domain {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.Data;
+
+@Data
+@Entity
+public class Product {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private long id; //기본키
+	
+	private long interestRate; //이율
+	
+	private String name; //상품 이름
+	
+	private long amount; //대출 금액
+	
+	private long repayPeriod; //상환기간
+	
+	private int ageType; //나이
 }
