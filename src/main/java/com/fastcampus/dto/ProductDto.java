06a4d39dc496ed9939b80d43e8e9a59e85a37fea
ddungi <imbc0224@naver.com>
2022-08-24 18:08:57 +0900

    feat : product dto 생성

--- src/main/java/com/fastcampus/dto/ProductDto.java
+++ src/main/java/com/fastcampus/dto/ProductDto.java
@@ -1,5 +1,19 @@
 package com.fastcampus.dto;
 
-public class Dto {
+import lombok.Data;
 
+@Data
+public class ProductDto {
+
+	private long id; // 기본키
+
+	private long interestRate; // 이율
+
+	private String name; // 상품 이름
+
+	private long amount; // 대출 금액
+
+	private long repayPeriod; // 상환기간
+
+	private int ageType; // 나이
 }
