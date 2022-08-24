a50a594fdaa76d774f18e4743938343f75bb6c74
ddungi <imbc0224@naver.com>
2022-08-24 13:56:56 +0900

    feat : start mini project

---
+++ src/main/java/com/fastcampus/FinProductRecoApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinProductRecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinProductRecoApplication.class, args);
+	}
+
+}
