615a12f272b5dd4b79f3074e1fa9c8b770bba765
ddungi <imbc0224@naver.com>
2022-08-24 18:09:18 +0900

    feat : product 레포지토리 생성

--- src/main/java/com/fastcampus/persistence/ProductRepository.java
+++ src/main/java/com/fastcampus/persistence/ProductRepository.java
@@ -1,5 +1,23 @@
 package com.fastcampus.persistence;
 
-public class Repository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.fastcampus.domain.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+	//찜한 상품 출력
+	@Query("select o from Product o where id in :ids")
+	List<Product> findByLikeList(@Param("ids") List<Long> likeList);
+
+	//상품 검색
+	List<Product> findByNameContainsIgnoreCase(String keyword);
+	
+	//나이에 맞는 삼품 검색
+	List<Product> findByAgeType(int ageType);
 
 }
