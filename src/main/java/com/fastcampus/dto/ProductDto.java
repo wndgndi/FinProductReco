5f3849d44fb220239a4002691d3a8c9514242708
ddungi <imbc0224@naver.com>
2022-08-25 16:51:58 +0900

    feat : ProductDto 업데이트

--- src/main/java/com/fastcampus/dto/ProductDto.java
+++ src/main/java/com/fastcampus/dto/ProductDto.java
@@ -5,15 +5,21 @@ import lombok.Data;
 @Data
 public class ProductDto {
 
-	private long id; // 기본키
-
-	private long interestRate; // 이율
-
-	private String name; // 상품 이름
-
-	private long amount; // 대출 금액
-
-	private long repayPeriod; // 상환기간
-
-	private int ageType; // 나이
+	private long id; //기본키
+	
+	private long interestRate; //이율
+	
+	private String name; //상품 이름
+	
+	private long amount; //대출 금액
+	
+	private long repayPeriod; //상환기간
+	
+	private String agency; //대출 기관
+	
+	private Long cartCount; //장바구니 등록 수
+	
+	private String job; //직업
+	
+	private int age; //나이
 }
