f344924d1fc810ddd2bd5fd02da72c4c996fca79
ddungi <imbc0224@naver.com>
2022-08-25 16:51:33 +0900

    feat : ProductController 기능 수정

--- src/main/java/com/fastcampus/controller/ProductController.java
+++ src/main/java/com/fastcampus/controller/ProductController.java
@@ -1,14 +1,13 @@
 package com.fastcampus.controller;
 
+import java.util.HashSet;
 import java.util.List;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fastcampus.domain.Product;
 import com.fastcampus.security.jpa.UserDetailsImpl;
@@ -22,43 +21,32 @@ public class ProductController {
 	
 	private final ProductService productService;
 	
-	//찜 등록
-	@PostMapping("/products/like/{id}")
-	public void postLikeProduct(@PathVariable long id,@AuthenticationPrincipal UserDetailsImpl userDetails) {
-		productService.postLikeProduct(id, userDetails);
-	}
-	
-	//찜 등록 해제
-	@DeleteMapping("/products/like/{id}")
-	public void deleteLikeProduct(@PathVariable long id,@AuthenticationPrincipal UserDetailsImpl userDetails) {
-		productService.deleteLikeProduct(id, userDetails);
-	}
-	
-	//찜 목록 조회
-	@GetMapping("/products/likes")
-	public void getLikeProduct(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-		List<Product> LikeProducts = productService.getLikeProducts(userDetails);
-		model.addAttribute("LikeProducts", LikeProducts);
-	}
-	
 	//모든 상품 조회
 	@GetMapping("/products")
-	public void getProductList(Model model) {
-		model.addAttribute("products", productService.getProducts());
+	@ResponseBody
+	public List<Product> getProductList() {
+		return productService.getProducts();
 	}
 	
 	//상품 검색
 	@GetMapping("/products/{keyword}")
-	public void getSearchedProductList(@PathVariable String keyword, Model model) {
-		model.addAttribute("SearchedProducts", productService.getSearchedProducts(keyword));
+	@ResponseBody
+	public List<Product> getSearchedProducts(@PathVariable String keyword) {
+		return productService.getSearchedProducts(keyword);
 	}
 	
 	//유저별 추천 상품 출력
 	@GetMapping("/products/recos")
-	public void getProductList(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-		List<Product> recoProducts= productService.getRecoProducts(userDetails);
-		model.addAttribute("recoProducts", recoProducts);
+	@ResponseBody
+	public List<Product> getRecoProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+		return productService.getRecoProducts(userDetails);
 	}
 	
+	//주력 상품 랜덤 추천
+	@GetMapping("/products/promotions")
+	@ResponseBody
+	public List<Product> getPromoProducts(){
+		return productService.getPromoProducts();
+	}
 	
 }
