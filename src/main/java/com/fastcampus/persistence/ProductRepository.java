cc8539c6345cc77bdde66668bcbc987c7d541e5e
ddungi <imbc0224@naver.com>
2022-08-25 16:52:44 +0900

    feat :  ProductRepository 나이, 직업별 조회 기능 추가

--- src/main/java/com/fastcampus/persistence/ProductRepository.java
+++ src/main/java/com/fastcampus/persistence/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.fastcampus.persistence;
 
+import java.util.HashSet;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,15 +10,17 @@ import org.springframework.data.repository.query.Param;
 import com.fastcampus.domain.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
-	
-	//찜한 상품 출력
-	@Query("select o from Product o where id in :ids")
-	List<Product> findByLikeList(@Param("ids") List<Long> likeList);
-
 	//상품 검색
 	List<Product> findByNameContainsIgnoreCase(String keyword);
 	
 	//나이에 맞는 삼품 검색
-	List<Product> findByAgeType(int ageType);
+	List<Product> findByAge(int age);
+	
+	//직업에 맞는 상품 검색
+	List<Product> findByJob(String job);
+
+	//랜덤 상품 출력
+	@Query("select o from Product o where id in :ids")
+	List<Product> findByRamdom(@Param("ids") HashSet<Integer> hashSet);
 
 }
