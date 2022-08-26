898d52ecae9c0e8c3811f2f4bbae126a183ef111
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-08-26 14:56:13 +0900

    feat : User 도메인 생성

---
+++ src/main/java/com/fastcampus/domain/User.java
@@ -0,0 +1,51 @@
+package com.fastcampus.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.GeneratorType;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_Id", nullable = false)
+	private Long id;   // 식별키
+	
+	@Column(name = "username", nullable = false, length = 50, unique = true)
+	private String username;    // 아이디
+	
+	@Column(name = "password", nullable = false)
+	private String password;    // 비밀번호
+	
+	@Column(name = "name", nullable = false)
+	private String name;    // 이름
+	
+	@Column(name = "role")
+	private String role;    // 권한
+		
+	@Column(name = "job")
+	private String job;     // 직업
+	
+	@Column(name = "age")
+	private int age;        // 나이
+	
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="CART_ID")
+	private Cart cart;
+
+}
