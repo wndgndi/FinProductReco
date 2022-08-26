775bf2a7fc47213ba733f648a1c3ff537510ea05
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-08-26 17:07:08 +0900

    refactor : User 수정

--- src/main/java/com/fastcampus/domain/User.java
+++ src/main/java/com/fastcampus/domain/User.java
@@ -1,28 +1,14 @@
 package com.fastcampus.domain;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
-
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-
-import javax.persistence.Table;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-
-
-import org.hibernate.annotations.GeneratorType;
-import lombok.Data;
+import javax.persistence.Table;
 
 import lombok.Data;
 
@@ -37,10 +23,10 @@ public class User extends BaseTime {
 	private Long id; //기본키
 
 	@Column(name = "username", nullable = false, length = 50, unique = true)
-	private Long username; //유저네임
+	private String username; //유저네임
 
 	@Column(name = "password", nullable = false)
-	private Long password; //비밀번호
+	private String password; //비밀번호
 
 	@Column(name = "name", nullable = false)
 	private String name; //유저 이름
