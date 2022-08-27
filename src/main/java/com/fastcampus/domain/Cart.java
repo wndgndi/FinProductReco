fe57805b95876638eaeb70d5d9495efee6a59972
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-08-27 15:41:37 +0900

    fix : mappedby 수정, OneToOne 어노테이션 추가

--- src/main/java/com/fastcampus/domain/Cart.java
+++ src/main/java/com/fastcampus/domain/Cart.java
@@ -1,15 +1,15 @@
-
-
 package com.fastcampus.domain;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fastcampus.persistence.CartRepository;
@@ -30,8 +30,11 @@ public class Cart {
     @Column(name = "cart_id")
 	private Long id;
 	
-	@OneToOne(mappedBy = "delivery", fetch = FetchType.EAGER)
+	@OneToMany(mappedBy = "cart", fetch = FetchType.LAZY)
 	private List<Product> products = new ArrayList<>();
+	
+	@OneToOne(mappedBy = "cart", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private User user;
 	
 	// 편의 메소드
 	// 객체에서 양방향 연관관계는 양쪽에 다 값을 세팅해야 하므로 편의 메소드로 한번에 세팅 
