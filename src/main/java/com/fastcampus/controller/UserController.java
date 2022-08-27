287c8492a8e14b2bac38cd4293f31e92ea0ec7d1
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-08-27 15:39:18 +0900

    feat : Swagger에 사용할 어노테이션 추가

--- src/main/java/com/fastcampus/controller/UserController.java
+++ src/main/java/com/fastcampus/controller/UserController.java
@@ -14,6 +14,7 @@ import com.fastcampus.dto.UserDto;
 import com.fastcampus.service.UserService;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
 @Api(tags = {"User 정보를 제공하는 Controller"})
@@ -27,18 +28,21 @@ public class UserController {
 
 	
 	// 회원가입
+	@ApiOperation(value = "회원가입")
 	@PostMapping("/users")
 	public void insertUser(@RequestBody UserDto userDto) {
 		userService.insertUser(userDto);
 	}
 	
 	// 회원 상세 조회
+	@ApiOperation(value = "회원 상세 조회")
 	@GetMapping("/user/{id}")
 	public UserDto getUser(@PathVariable Long id) {
 		return userService.getUser(id);
 	}
 	
 	// 회원 정보 수정
+	@ApiOperation(value = "회원 정보 수정")
 	@PutMapping("/user")
 	public void updateUser(@RequestBody UserDto userDto) {
 		 userService.updateUser(userDto);
