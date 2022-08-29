2071fdcac5377b57b9af8b103c02a995a624c809
ddungi <imbc0224@naver.com>
2022-08-29 23:35:06 +0900

    conf : 패키지 수정

--- src/main/java/com/fastcampus/config/WebConfig.java
+++ src/main/java/com/fastcampus/config/WebConfig.java
@@ -3,11 +3,10 @@ package com.fastcampus.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.fastcampus.jwt.JwtInterceptor;
+import com.fastcampus.jwt.interceptor.JwtInterceptor;
 
 import lombok.RequiredArgsConstructor;
 
