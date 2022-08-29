656c095beb135f84eae2fba17d6d139bd3faf413
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-08-29 16:24:20 +0900

    refactor : value 변경

--- src/main/java/com/fastcampus/dto/UserDto.java
+++ src/main/java/com/fastcampus/dto/UserDto.java
@@ -10,19 +10,19 @@ public class UserDto {
 
 private Long id;   // 식별키
 	
-	@ApiModelProperty(value = "회원 아이디", example = "asdfw", required = true )
+	@ApiModelProperty(value = "아이디", example = "asdfw", required = true )
 	private String username;    // 아이디
 	
-	@ApiModelProperty(value = "회원 비밀번호", example = "1q2w3e4r", required = true )
+	@ApiModelProperty(value = "비밀번호", example = "1q2w3e4r", required = true )
 	private String password;    // 비밀번호
 	
-	@ApiModelProperty(value = "회원 이름", example = "홍길동", required = true )
+	@ApiModelProperty(value = "이름", example = "홍길동", required = true )
 	private String name;    // 이름
 
-	@ApiModelProperty(value = "회원 직업", example = "직장인", required = true )
+	@ApiModelProperty(value = "직업", example = "직장인", required = true )
 	private JobType job;     // 직업
 
-	@ApiModelProperty(value = "회원 나이", example = "20", required = true )
+	@ApiModelProperty(value = "나이", example = "20", required = true )
 	private int age;        // 나이
 }
 
