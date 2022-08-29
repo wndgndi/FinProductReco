6e9b61d05d0dd08c527e91eeaddc7bed3cd1122e
ddungi <imbc0224@naver.com>
2022-08-29 16:06:33 +0900

    feat : result 객체 추가

---
+++ src/main/java/com/fastcampus/jwt/Result.java
@@ -0,0 +1,63 @@
+package com.fastcampus.jwt;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@NoArgsConstructor
+@Getter
+@ToString
+public class Result {
+    public static final String SUCCESS_MESSAGE = "성공";
+    public static final String SERVER_ERROR_MESSAGE = "실패";
+
+    private HttpStatus statusCode;
+    private String message;
+    private Object data;
+    private int totalCount;
+
+    public static Result successInstance(){
+        return new Result().success();
+    }
+
+    public Result success(){
+        statusCode = HttpStatus.OK;
+        message = SUCCESS_MESSAGE;
+        return this;
+    }
+
+    public Result fail(){
+        statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+        message = SERVER_ERROR_MESSAGE;
+        return this;
+    }
+
+    public Result setStatusCode(HttpStatus statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+
+    public Result setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public Result setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+        return this;
+    }
+
+    
+    public Result setData(Object data) {
+    	this.data = data;
+    	return this;
+    }
+    
+    public int getStatusCode() {
+        return statusCode.value();
+    }
+    
+}
