7281851da877e8e0e26de66053c4e6e104f4a8fe
ddungi <imbc0224@naver.com>
2022-08-29 16:05:29 +0900

    feat : jwtService 추가

---
+++ src/main/java/com/fastcampus/jwt/JwtService.java
@@ -0,0 +1,80 @@
+package com.fastcampus.jwt;
+
+import java.io.UnsupportedEncodingException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.fastcampus.domain.User;
+import com.fastcampus.jwt.exception.UnauthorizedException;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class JwtService{
+
+	private static final String SALT =  "orimujung";
+	
+    //JWT 생성
+	public <T> String create(String key, T data){
+		String jwt = Jwts.builder()
+					 .setHeaderParam("typ", "JWT")
+					 .setHeaderParam("regDate", System.currentTimeMillis())
+					 .claim(key, data)
+					 .signWith(SignatureAlgorithm.HS256, this.generateKey())
+					 .compact();
+		return jwt;
+	}	
+
+	//JWT Key 생성
+	private byte[] generateKey(){
+		byte[] key = null;
+		try {
+			key = SALT.getBytes("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("Making JWT Key Error");
+		}
+		return key;
+	}
+	
+	public boolean isUsable(String jwt) {
+		try{
+			Jws<Claims> claims = Jwts.parser()
+					  .setSigningKey(this.generateKey())
+					  .parseClaimsJws(jwt);
+			return true;
+			
+		}catch (Exception e) {
+			throw new UnauthorizedException();
+		}
+	}
+	
+	public Map<String, Object> get(String key) {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+		String jwt = request.getHeader("Authorization");
+		Jws<Claims> claims = null;
+		try {
+			claims = Jwts.parser()
+						 .setSigningKey(SALT.getBytes("UTF-8"))
+						 .parseClaimsJws(jwt);
+		} catch (Exception e) {
+			throw new UnauthorizedException();
+		}
+		@SuppressWarnings("unchecked")
+		Map<String, Object> value = (LinkedHashMap<String, Object>)claims.getBody().get(key);
+		return value;
+	}
+	
+	public User getUser() {
+		return (User) this.get("user");
+	}
+	
+}
