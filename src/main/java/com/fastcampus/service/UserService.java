35482ef1ae8e0e068d457632cc6efcfb545b4531
ddungi <imbc0224@naver.com>
2022-08-29 16:07:23 +0900

    feat : 업데이트 유저 서비스 로직

--- src/main/java/com/fastcampus/service/UserService.java
+++ src/main/java/com/fastcampus/service/UserService.java
@@ -1,12 +1,17 @@
 package com.fastcampus.service;
 
-import org.springframework.transaction.annotation.Transactional;
-import org.modelmapper.ModelMapper;
+import java.util.Objects;
+import java.util.Optional;
 
+import org.mindrot.jbcrypt.BCrypt;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.fastcampus.domain.User;
 import com.fastcampus.dto.UserDto;
 import com.fastcampus.persistence.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -16,11 +21,54 @@ public class UserService {
 	private final UserRepository userRepository;
 	private final ModelMapper modelMapper;
 	
-	// 회원가입
+	private static final String SIGNIN_EXCEPTION_MSG = "로그인정보가 일치하지 않습니다.";
+	private static final String USERNAME_EXIST_EXCEPTION_MSG = "이미 계정이 존재합니다.";
+	
+	//로그인
+	@Transactional
+	public UserDto login(String username, String password) {
+		User user = userRepository.findByUsername(username).get();
+		Objects.requireNonNull(user, SIGNIN_EXCEPTION_MSG);
+		
+		if( ! this.isAccordPassword(user, password)){
+			throw new IllegalStateException(SIGNIN_EXCEPTION_MSG);
+		}
+		UserDto checkedUserDto = modelMapper.map(user, UserDto.class);
+		
+		return checkedUserDto;
+	}
+	
+	//비밀번호 확인
+	private boolean isAccordPassword(User user, String password){
+		String encodedPassword = user.getPassword();
+		return BCrypt.checkpw(password, encodedPassword);
+	}
+	
+	// 회원가입(아이디 유효성 검사, 비밀번호 암호화 추가)
 	@Transactional
 	public void insertUser(UserDto userDto) {
+		this.validate(userDto.getUsername());
+		String encodedPassword =  BCrypt.hashpw(userDto.getPassword(), BCrypt.gensalt());
+		userDto.setPassword(encodedPassword);
 		User user = modelMapper.map(userDto, User.class);
 		userRepository.save(user);
+	}
+
+	//아이디 유효성 검사
+	public boolean isExist(String username) {
+		boolean isExist = false;
+		Optional<User> user = userRepository.findByUsername(username);
+		if(user.isPresent()){
+		    isExist = true;
+		}
+		return isExist;
+	}
+	
+	//아이디 예외 처리
+	public void validate(String username){
+		if( this.isExist(username) ){
+			throw new IllegalStateException(USERNAME_EXIST_EXCEPTION_MSG);
+		}
 	}
 	
 	// 유저 상세 조회
