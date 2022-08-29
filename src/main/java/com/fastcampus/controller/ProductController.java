c9948e9e7f26c13936df6a28ca74df3de5cf7a40
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-08-29 20:57:35 +0900

    feat : Swagger에 Product 컨트롤러 설명

--- src/main/java/com/fastcampus/controller/ProductController.java
+++ src/main/java/com/fastcampus/controller/ProductController.java
@@ -11,8 +11,12 @@ import com.fastcampus.domain.User;
 import com.fastcampus.dto.ProductDto;
 import com.fastcampus.service.ProductService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
+@Api(tags = {"Product 정보를 제공하는 Controller"})
 @RequiredArgsConstructor
 @Controller
 public class ProductController {
@@ -20,6 +24,7 @@ public class ProductController {
 	private final ProductService productService;
 	
 	//모든 상품 조회
+	@ApiOperation(value = "모든 상품 조회", notes = "등록된 모든 상품들을 조회한다.")
 	@GetMapping("/products")
 	@ResponseBody
 	public List<ProductDto> getProductList() {
@@ -27,6 +32,8 @@ public class ProductController {
 	}
 	
 	//상품 검색
+	@ApiOperation(value = "상품 검색", notes = "특정 키워드를 통해 상품을 검색한다.")
+	@ApiImplicitParam(name = "keyword", value = "검색을 위한 특정 키워드", dataType = "String", paramType = "path")
 	@GetMapping("/products/{keyword}")
 	@ResponseBody
 	public List<ProductDto> getSearchedProducts(@PathVariable String keyword) {
@@ -34,6 +41,7 @@ public class ProductController {
 	}
 	
 	//유저별 추천 상품 출력
+	@ApiOperation(value = "추천 상품", notes = "회원에 따라 적합한 상품을 추천해준다.")
 	@GetMapping("/products/recos")
 	@ResponseBody
 	public List<ProductDto> getRecoProducts(User user) {
@@ -41,6 +49,7 @@ public class ProductController {
 	}
 	
 	//주력 상품 랜덤 추천
+	@ApiOperation(value = "랜덤 상품 추천", notes = "해당 기관의 주력 상품을 랜덤으로 추천해준다.")
 	@GetMapping("/products/promotions")
 	@ResponseBody
 	public List<ProductDto> getPromoProducts(){
