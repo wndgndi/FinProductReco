d7a10c74f1bb434f688aa64ac5d29dcca9b59e2c
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-08-31 13:08:47 +0900

    refactor : JobType 수정

--- src/main/java/com/fastcampus/domain/JobType.java
+++ src/main/java/com/fastcampus/domain/JobType.java
@@ -11,22 +11,12 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 @Getter
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)    // Enum -> Json (프론트에 보낼때)
+@JsonFormat
 public enum JobType {
-	EMPLOYEE("직장인"),
-	UNIMPLOYED("무직"), 
-	BUSINESS("자영업자"), 
-	STUDENT("학생"), 
-	FREELANCER("프리랜서");    // FREELANCER = key (디비에 저장), 프리랜서 = name (UI에 노출)  
-
-	private String name;
-
-	public String getName() {
-		return name;
-	}
-	
-	@JsonCreator    // Json -> Enum (서버에 넣을때) 
-    public static JobType fromJson(@JsonProperty("key") String name) {
-        return valueOf(name);
-    }
+	EMPLOYEE,
+	UNEMPLOYED,
+	BUSINESS,
+	STUDENT,
+	FREELANCER;
 }
+	
