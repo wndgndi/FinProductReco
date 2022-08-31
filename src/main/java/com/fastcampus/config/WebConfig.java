96b1d4446497b9dff3ed2329cc836240a7163142
ddungi <imbc0224@naver.com>
2022-08-31 14:08:52 +0900

    conf : 임시적으로 언트랙

---
+++ src/main/java/com/fastcampus/config/WebConfig.java
@@ -0,0 +1,33 @@
+package com.fastcampus.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.fastcampus.jwt.interceptor.JwtInterceptor;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    //접근 제한 없는 URI
+	private static final String[] EXCLUDE_PATHS = {
+			"/login",
+            "/users",
+            "/error/**",
+            "/v2/api-docs",
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/products/**" 
+    };// 임시적인 허용
+    
+    private final JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(jwtInterceptor)
+						.addPathPatterns("/**")
+						.excludePathPatterns(EXCLUDE_PATHS);
+    }
+}
